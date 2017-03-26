8453828f4ba7137622c4e96fdb7e92bc8c269e20
NGnius <ngniusness@gmail.com>
2017-03-26 15:33:26 -0400

    Add ability to find first post

--- necroDetector.py
+++ necroDetector.py
@@ -1,1 +1,15 @@
-import scraper
+import scraper, os
+forumsToCheckFile = open(os.getcwd()+r"/Resources/ForumsToCheck.txt", "r")
+forums = forumsToCheckFile.readlines()
+forumsToCheckFile.close
+print (forums)
+
+for i in range(0,len(forums)):
+    scraper.page.ret(forums[i])
+    #the long thing here finds the link to the top post, as per the formating of the Robocraft forums
+    #this involves finding the first link above the "Viewing [int] topics ...", which is the most recently updated post in the forum (forum section, technically)
+    viewloc = scraper.wordSearcher.wordSearcher("Viewing",scraper.page.raw)[0]
+    firstPostLinkLoc = viewloc - scraper.wordSearcher.wordSearcher("href="[::-1], scraper.page.raw[viewloc:0:-1])[0]
+    firstPostLinkQuotes = scraper.wordSearcher.wordSearcher('"',scraper.page.raw[firstPostLinkLoc:viewloc])
+    firstPostLink = scraper.page.raw[firstPostLinkQuotes[0]+firstPostLinkLoc+len('"'):firstPostLinkQuotes[1]+firstPostLinkLoc]
+    print (firstPostLink)
