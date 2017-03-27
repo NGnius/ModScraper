7a19a9521ffd45232f65eaa672c5bb62b127ea51
NGnius <ngniusness@gmail.com>
2017-03-27 13:24:01 -0400

    Finish and test necro detector

--- necroDetector.py
+++ necroDetector.py
@@ -1,15 +1,28 @@
-import scraper, os
+import scraper, os, time
+dates = []
 forumsToCheckFile = open(os.getcwd()+r"/Resources/ForumsToCheck.txt", "r")
 forums = forumsToCheckFile.readlines()
 forumsToCheckFile.close
-print (forums)
-
-for i in range(0,len(forums)):
-    scraper.page.ret(forums[i])
-    #the long thing here finds the link to the top post, as per the formating of the Robocraft forums
-    #this involves finding the first link above the "Viewing [int] topics ...", which is the most recently updated post in the forum (forum section, technically)
-    viewloc = scraper.wordSearcher.wordSearcher("Viewing",scraper.page.raw)[0]
-    firstPostLinkLoc = viewloc - scraper.wordSearcher.wordSearcher("href="[::-1], scraper.page.raw[viewloc:0:-1])[0]
-    firstPostLinkQuotes = scraper.wordSearcher.wordSearcher('"',scraper.page.raw[firstPostLinkLoc:viewloc])
-    firstPostLink = scraper.page.raw[firstPostLinkQuotes[0]+firstPostLinkLoc+len('"'):firstPostLinkQuotes[1]+firstPostLinkLoc]
-    print (firstPostLink)
+def necroDetector()
+    for i in range(0,len(forums)):
+        necroPosts = []
+        scraper.page.ret(forums[i])
+        #the long thing here finds the link to the top post, as per the formating of the Robocraft forums
+        #this involves finding the first link above the "Viewing [int] topics ...", which is the most recently updated post in the forum (forum section, technically)
+        viewloc = scraper.wordSearcher.wordSearcher("Viewing",scraper.page.raw)[0]
+        firstPostLinkLoc = viewloc - scraper.wordSearcher.wordSearcher("href="[::-1], scraper.page.raw[viewloc:0:-1])[0]
+        firstPostLinkQuotes = scraper.wordSearcher.wordSearcher('"',scraper.page.raw[firstPostLinkLoc:viewloc])
+        firstPostLink = scraper.page.raw[firstPostLinkQuotes[0]+firstPostLinkLoc+len('"'):firstPostLinkQuotes[1]+firstPostLinkLoc]
+        print (firstPostLink)
+        scraper.page.ret(firstPostLink)
+        dateEnds = scraper.find(" at ")
+        dates = []
+        for i in range(len(dateEnds)):
+            try:
+                dates.append([time.strptime(scraper.page.text[dateEnds[i]-len("dd/mm/yyyy"):dateEnds[i]], "%d/%m/%Y"),dateEnds[i]])
+            except:
+                pass
+        if dates[0][0][0] != dates[-1][0][0] and dates[-2][0][0] != dates[-1][0][0]: #check if the years match for first and last post as well as last and second last post
+            print ("Boop: We have a necro from ", firstPostLink)
+            necroPosts.append(firstPostLink)
+    return necroPosts
