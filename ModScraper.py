343b566466e84eecd328310f35ba75c494032ca8
NGnius <ngniusness@gmail.com>
2017-09-02 22:01:43 -0400

    Improve logging

--- ModScraper.py
+++ ModScraper.py
@@ -6,31 +6,30 @@ import ForumsToCheck, scraper, config, log_it
 import necroDetector, profanityDetector, capsDetector
 page = scraper.pageClass()
 post = scraper.pageClass()
+log_it.logg("Startup successful", genus=["log", "debug"])
 
 def main(): #main function, goes through the all the stuff
     global page, post
     startTime = time.time() #for science!
+    log_it.logg("Starting scraping round", genus=["debug"])
     for f in ForumsToCheck.forums: #check each forum sequentially
-        print("Scraping "+ f) #debug
+        log_it.logg("Scraping section "+ f, genus=["debug"])
         page.ret(f)
         check = "all"
         if config.retrieveConfig("NecroDetection") == check or config.retrieveConfig("ProfanityDetection") == check or config.retrieveConfig("CapsTitleDetection") == check: #don't run if nothing wants it to
             pagePosts = page.findAllPosts()
             for p in pagePosts: #go through all the posts in section f and check them according to the config
                 post.ret(p)
-                log_it.logg("Scraping " + p)
+                log_it.logg("Scraping " + p, genus=["debug","verbose"])
                 if config.retrieveConfig("NecroDetection")==check:
                     if necroDetector.isNecro(post, config.retrieveConfig("NecroTimeDelta")):
-                        print(p, " has been bumped from the dead.")
-                        log_it.logg(p + " has been bumped from the dead.")
+                        log_it.logg(p + " has been bumped from the dead.", genus=["output"])
                 if config.retrieveConfig("ProfanityDetection")==check:
                     if profanityDetector.isProfanity(post, config.retrieveConfig("SwearThreshold")):
-                        print(p, " has been sworn in a lot.")
-                        log_it.logg(p + " has been sworn in a lot.")
+                        log_it.logg(p + " has been sworn in a lot.", genus=["output"])
                 if config.retrieveConfig("CapsTitleDetection")==check:
                     if capsDetector.isCapsTitle(post, config.retrieveConfig("CapsTitleThreshold")):
-                        print(p," has a lot of caps in the title.")
-                        log_it.logg(p + " has a lot of caps in the title.")
+                        log_it.logg(p + " has a lot of caps in the title.", genus=["output"])
         check = "first"
         if config.retrieveConfig("NecroDetection") == check or config.retrieveConfig("ProfanityDetection") == check or config.retrieveConfig("CapsTitleDetection") == check: #don't run if nothing wants it to
             p = page.findFirstPost() #check the first post in section f according to the config
@@ -38,20 +37,16 @@ def main(): #main function, goes through the all the stuff
             post.ret(p)
             if config.retrieveConfig("NecroDetection")==check:
                 if necroDetector.isNecro(post, config.retrieveConfig("NecroTimeDelta")):
-                    print(p, " has been bumped from the dead.")
-                    log_it.logg(p + " has been bumped from the dead.")
+                    log_it.logg(p + " has been bumped from the dead.", genus=["output"])
             if config.retrieveConfig("ProfanityDetection")==check:
                 if profanityDetector.isProfanity(post, config.retrieveConfig("SwearThreshold")):
-                    print(p, " has been sworn in a lot.")
-                    log_it.logg(p + " has been sworn in a lot.")
+                    log_it.logg(p + " has been sworn in a lot.", genus=["output"])
             if config.retrieveConfig("CapsTitleDetection")==check:
                 if capsDetector.isCapsTitle(post, config.retrieveConfig("CapsTitleThreshold")):
-                    print(p," has a lot of caps in the title.")
-                    log_it.logg(p + " has a lot of caps in the title.")
+                    log_it.logg(p + " has a lot of caps in the title.", genus=["output"])
 
     elapsedTime = time.time()-startTime #for science!
-    print ("Elapsed Time (s): ", elapsedTime) #for science!
-    log_it.logg ("Time elapsed in latest scrape: " + str(elapsedTime))
+    log_it.logg ("Time elapsed in latest scrape: " + str(elapsedTime), genus=["debug", "benchmarking"])
     if elapsedTime < config.retrieveConfig("Period"): #if the period specified in config hasn't elapsed completely
-        print("Sleeping") #debug
+        log_it.logg("Sleeping", genus=["debug"]) #debug
         time.sleep(config.retrieveConfig("Period")-elapsedTime) #sleep until the period time has elapsed
--- Resources/log_it.py
+++ Resources/log_it.py
@@ -2,13 +2,28 @@
 import sys, os, time
 sys.path.append(os.getcwd()+'/scraperResources')
 import fileIO
+sys.path.append(os.getcwd()+'/Resources')
+import config
 filename="log.txt"
-fileIO.addToFile(filename,"Starting up...\n", overwrite=True)
-run = True
 
 def timestamp():
     localtime = time.localtime()
     return "[" + str(localtime.tm_mday) + "/" + str(localtime.tm_mon) + "/" + str(localtime.tm_year) + " at " + str(localtime.tm_hour) + ":" + str(localtime.tm_min) + ":" + str(localtime.tm_sec) + "]"
 
-def logg(string): #write to the log
-    fileIO.addToFile(filename, timestamp()+string+"\n")
+def logg(string, genus=["debug"]): #write to the log
+    isin = True
+    for item in genus: #check for genuses in loggenuses
+        if item not in config.retrieveConfig("loggenuses"):
+            isin = False
+            break
+    if isin or "log" in genus:
+        fileIO.addToFile(filename, timestamp()+string+"\n")
+    isin = True
+    for item in genus: #check for genuses in printgenuses
+        if item not in config.retrieveConfig("printgenuses"):
+            isin = False
+            break
+    if isin or "print" in genus:
+        print(timestamp()+string)
+
+fileIO.addToFile(filename, timestamp()+"Starting up...\n", overwrite=True) #overwrite log file if it already exists, create it if not
