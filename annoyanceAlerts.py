e0172420da8c6962a9e0291ee7331fc04eafc3f4
NGnius <ngniusness@gmail.com>
2017-10-04 12:34:45 -0400

    Fix annoyance alerts search parameter order and simplify

--- annoyanceAlerts.py
+++ annoyanceAlerts.py
@@ -1,7 +1,7 @@
 def isCrusher(post):
     '''(pageClass) -> bool
     returns True if there is any mention of crusher in the post, False otherwise'''
-    if len(post.search(post.text, "crusher")) > 0 or len(post.search(post.text, "Crusher4881")) > 0:
+    if len(post.searchtext("crusher")) > 0 or len(post.searchtext("Crusher4881")) > 0:
         return True
     else:
         return False
@@ -9,7 +9,7 @@ def isCrusher(post):
 def isRotatingPlatforms(post):
     '''(pageClass) -> bool
     returns True is that horrible video link is found, False otherwise'''
-    if len(post.search(post.raw, "https://www.youtube.com/watch?v=uyEyppnE9c4&t=2s")) > 0 or len(post.search(post.raw, "https://youtu.be/uyEyppnE9c4")) > 0:
+    if len(post.searchraw("https://www.youtube.com/watch?v=uyEyppnE9c4")) > 0 or len(post.searchraw("https://youtu.be/uyEyppnE9c4")) > 0 or len(post.searchraw("uyEyppnE9c4")):
         return True
     else:
         return False
