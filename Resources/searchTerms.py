af35831a8d8ed33c05c1e733de453ff71d2b2981
NGnius <ngniusness@gmail.com>
2017-10-05 18:35:01 -0400

    add the docstrings I forgot two minutes ago

--- Resources/searchTerms.py
+++ Resources/searchTerms.py
@@ -1,6 +1,8 @@
 import scraper, os
 
 def loadTerms():
+    '''() -> list
+    loads each line of searchterms.txt as a seperate term'''
     terms = scraper.fileIO.retrieveFileLines(os.getcwd()+"/Resources/searchterms.txt")
     for i in range(len(terms)):
         terms[i] = terms[i].strip(" \n,.\t")
