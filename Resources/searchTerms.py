c9a68230bf71ea8399f89eb42ad07028089e95f4
NGnius <ngniusness@gmail.com>
2017-10-07 11:33:17 -0400

    Disable searchterms

--- Resources/searchTerms.py
+++ Resources/searchTerms.py
@@ -4,7 +4,10 @@ def loadTerms():
     '''() -> list
     loads each line of searchterms.txt as a seperate term'''
     terms = scraper.fileIO.retrieveFileLines(os.getcwd()+"/Resources/searchterms.txt")
+    output = []
     for i in range(len(terms)):
         terms[i] = terms[i].strip(" \n,.\t")
         terms[i] = terms[i].lower()
-    return terms
+        if len(terms[i])>1:
+            output.append(terms[i])
+    return output
